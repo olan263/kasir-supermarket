3d27a192024004458510a1c68cc18aed7ab31a45
Olan <olanrina9@gmail.com>
2026-04-29 20:58:14 +0700

    k

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,15 +4,27 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+/*
+|--------------------------------------------------------------------------
+| Vercel Compatibility Logic
+|--------------------------------------------------------------------------
+*/
+// Cek apakah aplikasi sedang berjalan di server Vercel
+if (env('VERCEL_JOB_ID') || env('NOW_REGION')) {
+    $app->useStoragePath('/tmp');
+}
+
+return $app;
