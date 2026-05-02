33d8d1f9eb3ce50213de21fc609470ce877c0544
Olan <olanrina9@gmail.com>
2026-05-02 22:31:20 +0700

    K:

--- api/index.php
+++ api/index.php
@@ -1,15 +1,25 @@
 <?php
-// Memanggil autoload dari root
+
+// 1. Aktifkan Debugging Paksa
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// 2. Memanggil autoload dari folder vendor di root
 require __DIR__ . '/../vendor/autoload.php';
 
-// Memanggil file bootstrap/app.php yang baru saja kamu edit
+// 3. Memanggil file bootstrap/app.php
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// 4. Proses Request menggunakan HTTP Kernel Laravel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
+// 5. Kirim Response ke Browser
 $response->send();
+
+// 6. Selesaikan Proses
 $kernel->terminate($request, $response);
