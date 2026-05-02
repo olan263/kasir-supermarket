778c33635a27e6ad291c1ad7a8134dde0fef3ff2
Olan <olanrina9@gmail.com>
2026-05-02 20:59:20 +0700

    K

--- api/index.php
+++ api/index.php
@@ -1,6 +1,13 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Jarak dari folder 'api' ke root adalah satu tingkat ke atas (../)
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-require __DIR__ . '/../public/index.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+$kernel->terminate($request, $response);
